2cb3a674e2cb02825384f62268e9cc4b92ed5559
Juan Hernandez <juanhernandez3119@gmail.com>
2023-01-30 22:47:01 -0800

    Made Exhibit and Banner. Asthetics non-existent

---
+++ sextant/src/components/banner.js
@@ -0,0 +1,10 @@
+import '../index.css';
+function Banner(){
+    return(
+        <div className="banner">
+            <h1>Welcome to Sextant</h1>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ sextant/src/components/exhibit.js
@@ -0,0 +1,9 @@
+function Exhibit(){
+    return(
+        <div className="exhibit">
+            <h1>Diagnostics</h1>
+        </div>
+    );
+}
+
+export default Exhibit;
