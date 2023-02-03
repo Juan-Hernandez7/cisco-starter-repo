93c738238f1f0013268bed3c50e4904e859a9707
Juan Hernandez <juanhernandez3119@gmail.com>
2023-02-03 00:06:06 -0800

    Latency component added: Calculates and displays latency

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Banner from './components/Banner';
 import Exhibit from './components/Exhibit';
 import IpDisplay from './components/IpDisplay';
+import Latency from './components/Latency';
+
 class App extends Component {
     render() {
         return (
@@ -13,6 +15,9 @@ class App extends Component {
                 </Exhibit>
                 <Exhibit name="Public IPv6 Address">
                     <IpDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name = "Packet Latency">
+                    <Latency/>
                 </Exhibit>
             </div>
         );
---
+++ sextant/src/components/Latency.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class Latency extends Component{
+    ws = new WebSocket('ws://localhost:55455');
+
+    constructor(props){
+        super(props);
+        this.state = {
+            delay: null
+        };    
+    }
+
+    componentDidMount(){
+
+        this.ws.onopen = () => {
+            console.log("open");
+        }
+
+        this.ws.onclose = () => {
+            console.log("close");
+        }
+
+        this.ws.onmessage = (event) => {
+            let time = Date.now() - event.data;
+            this.setState({delay:time});
+        }
+    }
+
+    render(){
+        return(
+            <div className="LatencyCheck">
+            <h3> {this.state.delay} ms</h3>
+
+            </div>
+        );
+    }
+}
+
+export default Latency;
